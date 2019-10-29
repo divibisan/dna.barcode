7a3e5288e9acb14aaa44b0583a8b7cdbe05d80be
Daniel Berman <divibisan@gmail.com>
2019-10-29 07:55:23 -0700

    1.0.5 - Add option for conensus sequence

--- R/make_barcode.R
+++ R/make_barcode.R
@@ -2,11 +2,19 @@
 #' @param seqs DNAStringSet containing sequences to align
 #' @param trim logical, should leading or trailing gaps be trimmed?
 #'   All sequences will be trimmed equally
+#' @param consensus logical, should a concensus sequence track be calculated?
 #' @importFrom magrittr %>%
 #' @return A data.frame in long-format, with 1 row per sequence and base
-make_dna_vectors <- function(seqs, trim = FALSE) {
+make_dna_vectors <- function(seqs, trim = FALSE, consensus = FALSE) {
   align <- msa(seqs, method = 'Muscle')
   align_list = msaConvert(align, type = "bios2mds::align")
+
+  if (consensus) {
+    cons <- msa::msaConsensusSequence(align) %>%
+      gsub('[ACTG]', 'M', .) %>%
+      strsplit(., '')
+    align_list <- c(align_list, 'Consensus' = cons)
+  }
 
   if (trim) {
     trim_end <- max(sapply(align_list, function(s) sum(rev(cumsum(rev(!(s == '-')))) == 0)))
@@ -28,12 +36,14 @@ make_dna_vectors <- function(seqs, trim = FALSE) {
 #'  Must match `rownames(sequences)`
 #' @param trim logical, should leading or trailing gaps be trimmed?
 #'  All sequences will be trimmed equally
+#' @param consensus logical, should a concensus sequence track be calculated and plotted?
 #' @return A ggplot containing a DNA barcode plot
 #' @importFrom magrittr %>%
 #' @export
-make_barcode_plot <- function(sequences, species_sub, trim = FALSE) {
-  dna_vectors <- make_dna_vectors(sequences[species_sub,], trim) %>%
-    mutate(species = factor(species, levels = rev(species_sub)))
+make_barcode_plot <- function(sequences, species_sub, trim = FALSE, consensus = FALSE) {
+  dna_vectors <- make_dna_vectors(sequences[species_sub,], trim, consensus = consensus) %>%
+    mutate(species = factor(species,
+                            levels = c(rev(species_sub), if (consensus) 'Consensus')))
 
   ggplot(dna_vectors, aes(x = rowid, y = species, fill = base)) +
     geom_tile(height = 0.9) +
@@ -42,7 +52,10 @@ make_barcode_plot <- function(sequences, species_sub, trim = FALSE) {
                                  'A' = rgb(20,42,250, max = 255),
                                  'C' = rgb(104, 243, 18, max = 255),
                                  'T' = rgb(230, 46, 37, max = 255),
-                                 'G' = rgb(0,0,0, max = 255))) +
+                                 'G' = rgb(0,0,0, max = 255),
+                                 'M' = 'grey40',
+                                 '?' = 'grey90'),
+                      breaks = c('A', 'C', 'T', 'G')) +
     scale_x_continuous(name = 'Sequence Position',
                        expand = c(0,0),
                        breaks = seq(0,max(dna_vectors$rowid), by = 100)) +
